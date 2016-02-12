edc3f3de32bf889a7f451df99de8a0ccb3585fba
Karmen Blake <dudeblake@gmail.com>
2016-02-11 20:42:55 -0800

    update connection agent

--- lib/neural_network/connection.ex
+++ lib/neural_network/connection.ex
@@ -2,7 +2,8 @@ defmodule NeuralNetwork.Connection do
   defstruct source: %{}, target: %{}, weight: 0.4 # make weight random at some point
 
   # def connection_for(source, target) do
-  #   {:ok, %NeuralNet.Connection{source: source, target: target}}
+
+  #   {:ok, %NeuralNetwork.Connection{source: source, target: target}}
   # end
 
   def start_link(name) do
@@ -11,5 +12,15 @@ defmodule NeuralNetwork.Connection do
 
   def get(name) do
     Agent.get(name, &(&1))
+  end
+
+  def update(name, key, value) do
+    Agent.update(name, fn connection ->
+      Map.put(%NeuralNetwork.Connection{}, key, value)
+    end)
+  end
+
+  def stop(name) do
+    Process.exit(Process.whereis(name), :shutdown)
   end
 end
--- test/neural_network/connection_test.exs
+++ test/neural_network/connection_test.exs
@@ -1,5 +1,5 @@
 defmodule NeuralNetwork.ConnectionTest do
-  use ExUnit.Case, async: true
+  use ExUnit.Case
   doctest NeuralNetwork.Connection
 
   test "has default values" do
@@ -15,5 +15,22 @@ defmodule NeuralNetwork.ConnectionTest do
     assert connection.source == %{}
     assert connection.target == %{}
     assert connection.weight == 0.4
+    NeuralNetwork.Connection.stop(:one)
+  end
+
+  test "update connection values" do
+    NeuralNetwork.Connection.start_link(:one)
+
+    NeuralNetwork.Connection.update(:one, :source, %NeuralNetwork.Neuron{input: 10})
+    connection = NeuralNetwork.Connection.get(:one)
+    assert is_map(connection.source)
+    assert connection.source.input == 10
+
+    NeuralNetwork.Connection.update(:one, :target, %NeuralNetwork.Neuron{input: 5})
+    connection = NeuralNetwork.Connection.get(:one)
+    assert is_map(connection.target)
+    assert connection.target.input == 5
+
+    NeuralNetwork.Connection.stop(:one)
   end
 end
