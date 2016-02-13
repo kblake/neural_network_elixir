94f0854252a6c53caa1666fb3c87ad8305b46181
Karmen Blake <dudeblake@gmail.com>
2016-02-12 17:29:36 -0800

    Add code to exercise the network learning code

---
+++ lib/mix/tasks/learn.ex
@@ -0,0 +1,80 @@
+defmodule Mix.Tasks.Learn do
+  use Mix.Task
+
+  @shortdoc "Run the neural network app"
+
+  def run(args) do
+    #neuronA = %NeuralNet.Neuron{}
+    #neuronB = %NeuralNet.Neuron{}
+
+    # {:ok, neuronA, neuronB} = NeuralNet.Neuron.connect(neuronA, neuronB)
+
+    #neuronA = NeuralNet.Neuron.activate(neuronA)
+    #IO.puts neuronA.output
+
+    #neuronB = NeuralNet.Neuron.activate(neuronB, 1)
+    #IO.puts neuronB.output
+
+    #layer = %NeuralNet.Layer{neurons: [%NeuralNet.Neuron{}, %NeuralNet.Neuron{}]}
+    #IO.inspect layer.neurons
+
+    #inputs = [1,1,1]
+    # {:ok, layer} = NeuralNet.Layer.activate(layer, inputs)
+
+    #IO.inspect layer.neurons
+
+    #input_layer = %NeuralNet.Layer{neurons: [%NeuralNet.Neuron{}, %NeuralNet.Neuron{}]}
+    #output_layer = %NeuralNet.Layer{neurons: [%NeuralNet.Neuron{}, %NeuralNet.Neuron{}]}
+
+    # {:ok, input_layer, output_layer} = %NeuralNet.Layer.connect(input_layer, output_layer)
+
+    #############################################################################
+    #############################################################################
+
+  #   neuronA = NeuralNetwork::Neuron.new
+  #   neuronB = NeuralNetwork::Neuron.new
+
+  #   neuronA.connect(neuronB)
+
+  #   10000.times do |n|
+  #     neuronA.activate(2)
+  #     neuronB.activate
+
+  #     #puts "A out: #{neuronA.output}"
+  #     #puts "B out: #{neuronB.output}"
+
+  #     neuronB.train(1)
+  #     neuronA.train
+
+  #     if n == 0 || n % 1000 == 0
+  #      puts "epoch: #{n} error: #{neuronB.error}"
+  #     end
+  #     #puts "A error/delta: #{neuronA.error}/#{neuronA.delta}"
+  #     #puts "B error/delta: #{neuronB.error}/#{neuronB.delta}"
+  #   end
+
+
+    NeuralNetwork.Neuron.start_link(:a)
+    NeuralNetwork.Neuron.start_link(:b)
+    neuronA = NeuralNetwork.Neuron.get((:a))
+    neuronB = NeuralNetwork.Neuron.get((:b))
+
+
+     {:ok, neuronA, neuronB} = NeuralNet.Neuron.connect(neuronA, neuronB)
+    #  IO.puts List.first(neuronB.incoming).source.output
+    #  IO.puts List.first(neuronB.incoming).weight
+
+    for i <- 1..1 do
+      IO.puts "neuronA activate =============="
+      neuronA = NeuralNet.Neuron.activate(neuronA, 2)
+      IO.puts "neuronA output: #{neuronA.output}"
+
+      IO.puts "neuronB activate =============="
+      neuronB = NeuralNet.Neuron.activate(neuronB)
+
+      # IO.puts "A out: #{neuronA.output}"
+      IO.puts "B out: #{neuronB.output}"
+    end
+
+  end
+end
