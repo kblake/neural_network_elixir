6b643c6f53dbb6f67bdaa73923a6c64d0063ccc8
Karmen Blake <dudeblake@gmail.com>
2017-11-01 19:33:01 -0700

    Update credo

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule NeuralNetwork.Mixfile do
       {:earmark, "~> 1.0.2", only: :dev},
       {:ex_doc, "~> 0.14.3", only: :dev},
       {:mix_test_watch, "~> 0.2", only: :dev},
-      {:credo, "~> 0.4", only: [:dev, :test]}
+      {:credo, "~> 0.8", only: [:dev, :test], runtime: false}
     ]
   end
 end
