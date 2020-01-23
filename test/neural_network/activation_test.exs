c1bf319c4d0260225ee1100940106e0df4a1c9fa
Marcelo Reichert <marcelo@powertask.com.br>
2020-01-23 01:07:35 -0300

    Module Activation test

--- test/neural_network/activation_test.exs
+++ test/neural_network/activation_test.exs
@@ -7,4 +7,13 @@ defmodule NeuralNetwork.ActivationTest do
   test ".activation_function sigmoid" do
     assert Activation.calculate_output(1, :sigmoid) == 0.7310585786300049
   end
+
+  test ".activation_function relu" do
+    assert Activation.calculate_output(1, :relu) == 1
+    assert Activation.calculate_output(-1, :relu) == 0
+  end
+
+  test ".activation_function softmax" do
+    assert Activation.calculate_output([1,2,3], :softmax) == [0.09003057317038046, 0.24472847105479764, 0.6652409557748218]
+  end
 end
